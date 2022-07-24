4ddd031b5315382bc623a247716758be3a2783b9
Julia Yadova <yadova.julia@yandex.ru>
2022-07-24 20:03:03 +0300

    Requirements new

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,8 +1,8 @@
-from django_filters.rest_framework import FilterSet, filters
+from django_filters import rest_framework as filters
 from recipes.models import Recipe
 
 
-class RecipeFilter(FilterSet):
+class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
