bb9eeed543ab45a0a0df19052846bd94336f0710
Julia Yadova <yadova.julia@yandex.ru>
2022-07-25 21:09:23 +0300

    fix artefact after del

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from recipes.views import FollowViewSet, favorites, shoping_list, subscriptions
+from recipes.views import FollowViewSet, subscriptions
 from rest_framework.routers import DefaultRouter
 
 app_name = 'users'
@@ -11,8 +11,6 @@ router_v1.register('users', FollowViewSet, basename='users')
 
 urlpatterns = [
     path('users/subscriptions/', subscriptions),
-    path('users/favorites/', favorites),
-    path('users/shoping_list/', shoping_list),
     path('', include('djoser.urls')),
     path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
